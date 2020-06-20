0f29bfeaaa8dc32a09f6820a4536acf9d919ddd8
Carlos Valencia <carlos@rizeen.com>
2020-06-19 23:31:04 -0500

    Handled transaction saving

--- controller/balance.js
+++ controller/balance.js
@@ -12,7 +12,7 @@ const soap = require('soap'),
 exports.addBalance = (req, res) => {
     const twiml = new MessagingResponse();
     //const enteredPhoneNumber = req.body.Body;
-    const enteredPhoneNumber = "4804340203";
+    const enteredPhoneNumber = "9911083007";
     const nakedPhoneNumber = enteredPhoneNumber.replace(/[^0-9]/g, '');
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
@@ -20,90 +20,76 @@ exports.addBalance = (req, res) => {
     if (nakedPhoneNumber.length === 10) {
         simController.existsSim(nakedPhoneNumber)
             .then(sim => {
-                if (nakedPhoneNumber === "4804340203") {
-                    twiml.message(enteredPhoneNumber + ' no está dado de alta para recargas automáticas.');
+                if (sim.active === true) {
+                    console.log("El número ya ha sido activado");
+                    twiml.message('El número ' + enteredPhoneNumber + ' ya ha sido activado. verifíque que el número sea correcto e inténtelo de nuevo. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
                     res.end(twiml.toString());
                 } else {
-        
+                    console.log("Procesando recarga");
+
                     const args = {
                         APIKey: process.env.RECARGAS_API_KEY,
                         ProductCode: PRODUCT_CODE,
                         Amount: AMOUNT,
                         PhoneNumber: nakedPhoneNumber
                     };
-        
-        
-        
-        
-        
-        
-        
-                    /**
-                     ***** A ver si sí es así 
-                     */
+
                     const transaction = new Transaction();
-        
-                    transaction.date = '',
-                    transaction.phone_number = '',
-                    transaction.distributor_id = '',
-                    transaction.transaction_number = '',
-                    transaction.transaction_id = '',
-                    transaction.amount = '',
-                    transaction.return_code = '',
-                    transaction.succes = ''
-        
-                    // save the user and check for errors
-                    transaction.save((err) => {
-                        if (err) {
-                            res.json(err);
-                        } else {
-                            
-                            // Saved succesfully 
-                            
-                        }
+
+                    soap.createClient(process.env.RECARGAS_API_URL, { wsdl_options: { timeout: 70000 }  },function(err, client) {
+                        client.TAERequest(args, function(err, result) {
+                            if (err) {
+                                transaction.success = false;
+                                transaction.return_code = 'Error';
+                                transaction.transaction_id = 'Error';
+                                transaction.transaction_number = 'Error';
+                                twiml.message('Por el momento el sistema no está disponible. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
+                                res.end(twiml.toString());
+                                console.log(err);
+                            } else {
+                                transaction.return_code = result.return.ReturnCode['$value'];
+                                transaction.transaction_id = result.return.IDTransaction['$value'];
+                                transaction.transaction_number = result.return.TransactionNumber['$value'];
+                                if (result.return.ReturnCode['$value'] === '0') {
+                                    console.log("Recarga exitosa");
+                                    transaction.success = true;
+                                    twiml.message('Se ha abonado $' + result.return.Amount['$value'] + '.00 MXN de saldo a la linea ' + result.return.PhoneNumber['$value'] + ' con el folio ' + result.return.TransactionNumber['$value']);
+                                } else {
+                                    console.log("Error al realizar recarga");
+                                    transaction.success = false;
+                                    twiml.message('Por el momento el sistema no está disponible. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
+                                }
+                                res.end(twiml.toString());
+                            }
+
+                            transaction.date = Date.now();
+                            transaction.amount = AMOUNT;
+                            transaction.phone_number = nakedPhoneNumber;
+                            transaction.distributor_id = sim.distributor_id;
+
+                            transaction.save((err) => {
+                                if (err) {
+                                    console.log(err)
+                                } else {
+                                    console.log("Success saving transaction")
+                                }
+                            });
+                        })
                     });
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-                    res.end("Transaction exitosa");
-        
-                    // soap.createClient(process.env.RECARGAS_API_URL, { wsdl_options: { timeout: 70000 }  },function(err, client) {
-                    //     client.TAERequest(args, function(err, result) {
-                    //         if (err) {
-                    //             twiml.message('Por el momento el sistema no está disponible. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
-                    //             res.end(twiml.toString());
-                    //             console.log(err);
-                    //         } else {
-                    //             if (result.return.ReturnCode['$value'] === '0') {
-                    //                 twiml.message('Se ha abonado $' + result.return.Amount['$value'] + '.00 MXN de saldo a la linea ' + result.return.PhoneNumber['$value'] + ' con el folio ' + result.return.TransactionNumber['$value']);
-                    //             } else {
-                    //                 twiml.message('Por el momento el sistema no está disponible. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
-                    //             }
-                    //             res.end(twiml.toString());
-                    //         }
-                    //     })
-                    // });
                 }
             })
             .catch(error => {
                 if (error === 404) {
-                    res.sendStatus(error)
+                    twiml.message(enteredPhoneNumber + ' no está dado de alta para recargas automáticas.');
                 } else {
-                    // Error whit database
-                    res.json(error)
+                    twiml.message('Por el momento el sistema no está disponible. Para más información comuníquese al teléfono: ' + CUSTOMER_SERVICE_NUMBER);
                 }
+                console.log(error);
+                res.end(twiml.toString());
             })
     } else {
-        twiml.message(enteredPhoneNumber + ' no es un número de teléfono válido',);
+        console.log("Número de teléfono no valido");
+        twiml.message(enteredPhoneNumber + ' no es un número de teléfono válido');
         res.end(twiml.toString());
     }
 };
