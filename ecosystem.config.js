5927e097fcae7ff789b35d976079292cc84c6bb8
Carlos Valencia <carlos@rizeen.com>
2020-07-04 20:43:16 -0500

    Added pm2 deployment rules

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+        name: 'sim-card-manager',
+        script: './server.js'
+    }],
+    deploy: {
+        production: {
+            user: 'carlos',
+            host: 'http://35.208.12.243/',
+            ref: 'origin/master',
+            repo: 'git@gitlab.com:magna-comunicaciones/sim-card-manager.git',
+            path: '/home/carlos/sim-card-manager',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+};
